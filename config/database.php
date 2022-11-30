7e7a9ad62ed19015f55014036484f44e495791d9
Sharif <developersharif@yahoo.com>
2022-11-30 13:06:59 +0600

    db nam removed

--- config/database.php
+++ config/database.php
@@ -14,7 +14,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'personal_cms'),
+            'database' => env('DB_DATABASE', ''),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
