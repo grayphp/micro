0454078c0c70ec6fce19ef57ef157e595fe91557
Sharif <developersharif@yahoo.com>
2022-11-30 13:22:12 +0600

    welcome page updated

--- resources/views/welcome.php
+++ resources/views/welcome.php
@@ -1,1 +1,16 @@
-Welcome page.
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome::Micro</title>
+    <link rel="stylesheet" href="<?php asset('css/style.css'); ?>">
+</head>
+
+<body>
+    <h1 data-heading="WELCOME">WELCOME TO <a href="https://github.com/grayphp/micro#readme">MICRO</a></h1>
+</body>
+
+</html>
